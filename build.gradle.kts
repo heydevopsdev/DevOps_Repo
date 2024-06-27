74d5f1aec26a9eaf6292f3a13916a7e4ef3f08e2
Kushagra Agarwal <138867347+kushagra023@users.noreply.github.com>
2024-06-28 01:48:32 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    id("com.android.application") version "8.4.1"
-    id("org.jetbrains.kotlin.android") version "1.9.0"
-    id("com.google.gms.google-services") version "4.4.2"
     id("org.sonarqube") version "3.3"
+    id("com.android.application") version "8.4.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
 
