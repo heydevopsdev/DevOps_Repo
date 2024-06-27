9b55a35f543f030b68764743068fedc80b9e481d
Kushagra Agarwal <138867347+kushagra023@users.noreply.github.com>
2024-06-28 01:42:06 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,15 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.4.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    id("com.google.gms.google-services") version "4.4.2" apply false
-    id 'org.sonarqube' version '3.3'
+    id("com.android.application") version "8.4.1"
+    id("org.jetbrains.kotlin.android") version "1.9.0"
+    id("com.google.gms.google-services") version "4.4.2"
+    id("org.sonarqube") version "3.3"
 }
 
+
+// SonarQube configuration
+configure<org.sonarqube.gradle.SonarQubeExtension> {
+    properties {
+        property("sonar.host.url", "http://43.205.138.85:9000")
+        property("sonar.login", System.getenv("sonarqube") ?: "")
+    }
+}
